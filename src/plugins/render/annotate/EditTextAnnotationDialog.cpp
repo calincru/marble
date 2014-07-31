e785cac1d3855001ef18faf063cb25d5a2a044f5
Cruceru Calin-Cristian <crucerucalincristian@gmail.com>
2014-07-31 18:42:15 +0300

    Simplified code a little bit

--- src/plugins/render/annotate/EditTextAnnotationDialog.cpp
+++ src/plugins/render/annotate/EditTextAnnotationDialog.cpp
@@ -147,9 +147,8 @@ EditTextAnnotationDialog::EditTextAnnotationDialog( PlacemarkTextAnnotation *tex
     connect( d->m_iconColorDialog, SIGNAL(colorSelected(QColor)), this, SLOT(updateTextAnnotation()) );
 
 
-    // Promote "Ok" button to default button and connect it to updateTextAnnotation() slot.
-    QPushButton *okButton = d->buttonBox->button( QDialogButtonBox::Ok );
-    okButton->setDefault( true );
+    // Promote "Ok" button to default button.
+    d->buttonBox->button( QDialogButtonBox::Ok )->setDefault( true );
 
     connect( d->m_browseButton, SIGNAL(pressed()), this, SLOT(loadIconFile()) );
     connect( d->buttonBox->button( QDialogButtonBox::Ok ), SIGNAL(pressed()), this, SLOT(checkFields()) );
